5123844890a63548c745205b3ae9858de5e25cf5
RainbowPawz <Rainbowzchild@gmail.com>
2015-02-03 12:57:31 -0700

    Recursive fix
    GetValue() now recursive.

--- sourceFiles/utopiantree.java
+++ sourceFiles/utopiantree.java
@@ -6,31 +6,34 @@ import java.util.regex.*;
 
 public class Solution {
 
-    static int getValues(int N)
+    /* 0  1  2  3  4  5  6  7  8  9  10  11  12
+       1  2  3  6  7  14 15 30 31 62 63  126 125 
+         1  1  3  1  7  1  15  1 31 1  63   1       current
+           2  4     8     16    32 */
+    static int getValues(int N, int value)
     {
-        int newHeight = N;
-        int newHeight2 = 0;
-        int current = 0;
-        
-        for(int i = 0; i <= newHeight; i++) 
-        {        
-            if(i == 0){
-                newHeight2 = 1; 
-                current = 1;
-            } 
+        int index = N;
+        int newIndex = 0;
+        int current = value;
 
-            else if(current % 2 == 0) {
-                newHeight2 = current + 1;
-                current = newHeight2;
+        if(index == 0)
+        {
+            return  current;
+        } 
+        else if( index > newIndex)
+        { 
+            if(current % 2 == 0) 
+            {
+                index--;
+                return getValues(index, current + 1);
             }
-                
             else
             {
-                newHeight2 = current * 2; 
-                current = newHeight2;
+                index--;
+                return getValues(index, current * 2);
             }
         }
-        return newHeight2;
+        return current;
     }
     public static void main(String[] args) {
         
@@ -48,7 +51,7 @@ public class Solution {
         }
     
         for(int i = 0; i < N; i++) {
-            System.out.println(getValues(inputs[i]));
+            System.out.println(getValues(inputs[i], 1));
         }
 
     }
